797af894e7b1b3bcefa04f45069ccf75f45b2299
hidden3110 <sc8941@nyu.edu>
2023-04-27 12:33:01 -0400

    FINAL

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+# USED THE CODE GIVEN MY PROFESSOR AT DOME POINT BUT IT FUNCTIONS DIFFERENTLY.
 #!/usr/bin/env python3
 from flask import Flask, render_template, request, redirect, url_for, make_response
 from markupsafe import escape
@@ -10,8 +10,6 @@ import subprocess
 # instantiate the app
 app = Flask(__name__)
 
-# load credentials and configuration options from .env file
-# if you do not yet have a file named .env, make one based on the template in env.example
 import credentials
 config = credentials.get()
 
@@ -31,38 +29,26 @@ db = connection[config['MONGO_DBNAME']] # store a reference to the database
 
 @app.route('/')
 def home():
-    """
-    Route for the home page.
-    Simply returns to the browser the content of the index.html file located in the templates folder.
-    """
+ 
     return render_template('index.html')
 
 
 @app.route('/read')
 def read():
-    """
-    Route for GET requests to the read page.
-    Displays some information for the user with links to other pages.
-    """
+  
     docs = db.boston1.find({}).sort("created_at", -1) # sort in descending order of created_at timestamp
     return render_template('read.html', docs=docs) # render the read template
 
 
 @app.route('/create')
 def create():
-    """
-    Route for GET requests to the create page.
-    Displays a form users can fill out to create a new document.
-    """
+    
     return render_template('create.html') # render the create template
 
 
 @app.route('/create', methods=['POST'])
 def create_post():
-    """
-    Route for POST requests to the create page.
-    Accepts the form submission data for a new document and saves the document to the database.
-    """
+   
     name = request.form['name']
     place = request.form['place']
     address = request.form['address']
@@ -81,20 +67,14 @@ def create_post():
 
 @app.route('/edit/<mongoid>')
 def edit(mongoid):
-    """
-    Route for GET requests to the edit page.
-    Displays a form users can fill out to edit an existing record.
-    """
+   
     doc = db.boston1.find_one({"_id": ObjectId(mongoid)})
     return render_template('edit.html', mongoid=mongoid, doc=doc) # render the edit template
 
 
 @app.route('/delete/<mongoid>')
 def delete(mongoid):
-    """
-    Route for GET requests to the delete page.
-    Deletes the specified record from the database, and then redirects the browser to the read page.
-    """
+   
     db.boston1.delete_one({"_id": ObjectId(mongoid)})
     return redirect(url_for('read')) # tell the web browser to make a request for the /read route.
 
